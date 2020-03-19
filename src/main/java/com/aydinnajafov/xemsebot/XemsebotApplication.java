fc92928b605d803cd770bf98fdbfe787629b7270
ajafov98 <aydinnecefov@gmail.com>
2020-03-19 23:09:24 +0400

    Initial commit

---
+++ src/main/java/com/aydinnajafov/xemsebot/XemsebotApplication.java
@@ -0,0 +1,13 @@
+package com.aydinnajafov.xemsebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XemsebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XemsebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aydinnajafov/xemsebot/XemsebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydinnajafov.xemsebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XemsebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
